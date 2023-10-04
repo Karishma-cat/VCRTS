78e8a65851c6726c7319d89db286b03597d6654c
Tiffany <tiffanyrhale1@gmail.com>
2023-10-04 13:51:12 -0400

    Started styling the GUI by making the text pink (temporary colors)

--- VRCTSJFrame.java
+++ VRCTSJFrame.java
@@ -1,3 +1,8 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,53 +13,53 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
-public class VRCTSJFrame 
-{
+public class VRCTSJFrame {
     private static JFrame frame;
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         initializeGUI();
     }
 
-    private static void initializeGUI() 
-    {
-        frame = new JFrame();
-        frame.setSize(375, 300);
-        frame.setTitle("Vehicle Cloud Real Time System");
+    private static void initializeGUI() {
+        JFrame frame = new JFrame("Vehicle Cloud Real Time System");
+        frame.setSize(400, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel label1 = new JLabel("Welcome to the Vehicular Cloud Real Time System");
-        label1.setBounds(50, 50, 400, 30);
-        frame.add(label1);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        frame.add(mainPanel);
 
-        JLabel label2 = new JLabel("Please select one of the options below:");
-        label2.setBounds(50, 80, 400, 30);
-        frame.add(label2);
+        JLabel welcomeLabel = createStyledLabel("Welcome to the Vehicular Cloud Real Time System");
+        welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
+        mainPanel.add(welcomeLabel, BorderLayout.NORTH);
 
-        JButton jobButton = new JButton("Submit a Job");
-        jobButton.setBounds(50, 120, 250, 30);
-        frame.add(jobButton);
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        mainPanel.add(buttonPanel, BorderLayout.CENTER);
 
-        JButton ownerButton = new JButton("Login to Owner Panel");
-        ownerButton.setBounds(50, 160, 250, 30);
-        frame.add(ownerButton);
+        JLabel messageLabel = createStyledLabel("Please select one of the options below:");
+        buttonPanel.add(messageLabel);
 
-        frame.setLayout(null);
+        JButton jobButton = createStyledButton("Submit a Job");
+        buttonPanel.add(jobButton);
+
+        JButton ownerButton = createStyledButton("Login to Owner Panel");
+        buttonPanel.add(ownerButton);
+
         frame.setVisible(true);
 
         jobButton.addActionListener(e -> openJobSubmission());
         ownerButton.addActionListener(f -> openOwnerPanel());
     }
 
-    private static void openJobSubmission() 
-    {
+    private static void openJobSubmission() {
         JFrame jobFrame = new JFrame("Job Submission");
         jobFrame.setSize(300, 350);
 
-        JLabel jobLabel = new JLabel("Client ID:");
+        JLabel jobLabel = createStyledLabel("Client ID:");
         jobLabel.setBounds(20, 20, 200, 30);
         jobFrame.add(jobLabel);
 
@@ -62,7 +67,7 @@ public class VRCTSJFrame
         clientIdField.setBounds(20, 60, 200, 30);
         jobFrame.add(clientIdField);
 
-        JLabel jobDuration = new JLabel("Approximate duration of task:");
+        JLabel jobDuration = createStyledLabel("Approximate duration of task:");
         jobDuration.setBounds(20, 90, 200, 30);
         jobFrame.add(jobDuration);
 
@@ -70,7 +75,7 @@ public class VRCTSJFrame
         jobDurationTextField.setBounds(20, 120, 200, 30);
         jobFrame.add(jobDurationTextField);
 
-        JLabel jobDeadline = new JLabel("Job Deadline: (mm/dd/yyyy)");
+        JLabel jobDeadline = createStyledLabel("Job Deadline: (mm/dd/yyyy)");
         jobDeadline.setBounds(20, 150, 200, 30);
         jobFrame.add(jobDeadline);
 
@@ -85,8 +90,7 @@ public class VRCTSJFrame
         jobFrame.setLayout(null);
         jobFrame.setVisible(true);
 
-        submitJobButton.addActionListener(event -> 
-        {
+        submitJobButton.addActionListener(event -> {
             String clientID = clientIdField.getText();
             LocalDateTime currentTime = LocalDateTime.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -104,12 +108,11 @@ public class VRCTSJFrame
         });
     }
 
-    private static void openOwnerPanel() 
-    {
+    private static void openOwnerPanel() {
         JFrame ownerFrame = new JFrame("Owner Panel");
         ownerFrame.setSize(300, 350);
 
-        JLabel ownerIDLabel = new JLabel("Owner ID:");
+        JLabel ownerIDLabel = createStyledLabel("Owner ID:");
         ownerIDLabel.setBounds(20, 20, 200, 30);
         ownerFrame.add(ownerIDLabel);
 
@@ -117,7 +120,7 @@ public class VRCTSJFrame
         ownerIDTextField.setBounds(20, 60, 200, 30);
         ownerFrame.add(ownerIDTextField);
 
-        JLabel vehicleInfo = new JLabel("Vehicle Information:");
+        JLabel vehicleInfo = createStyledLabel("Vehicle Information:");
         vehicleInfo.setBounds(20, 90, 200, 30);
         ownerFrame.add(vehicleInfo);
 
@@ -125,7 +128,7 @@ public class VRCTSJFrame
         vehicleInfoTextField.setBounds(20, 120, 200, 30);
         ownerFrame.add(vehicleInfoTextField);
 
-        JLabel residentTime = new JLabel("Residency Time:");
+        JLabel residentTime = createStyledLabel("Residency Time:");
         residentTime.setBounds(20, 150, 200, 30);
         ownerFrame.add(residentTime);
 
@@ -140,8 +143,7 @@ public class VRCTSJFrame
         ownerFrame.setLayout(null);
         ownerFrame.setVisible(true);
 
-        submitOwnerInfoButton.addActionListener(event -> 
-        {
+        submitOwnerInfoButton.addActionListener(event -> {
             String ownerID = ownerIDTextField.getText();
             LocalDateTime currentTime = LocalDateTime.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("mm-dd-yyyy HH:mm:ss");
@@ -159,18 +161,45 @@ public class VRCTSJFrame
         });
     }
 
-    private static boolean writeToFile(String data, String fileName) 
-    {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) 
-        {
+    private static boolean writeToFile(String data, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
             writer.write(data);
             writer.newLine();
             return true;
-        } 
-        catch (IOException e) 
-        {
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
+    }
+
+    private static JButton createStyledButton(String text) 
+    {
+        JButton button = new JButton(text);
+
+        button.setPreferredSize(new Dimension(200, 40));
+        button.setFont(new Font("Arial", Font.BOLD, 14));
+
+        button.setBackground(new Color(255,255,255));
+
+        button.setForeground(Color.PINK);
+
+        LineBorder border = new LineBorder(Color.PINK, 2);
+        button.setBorder(border);
+
+        button.setFocusPainted(false);
+        button.setBorderPainted(true); 
+        button.setOpaque(true); 
+
+        return button;
+    }
+
+    private static JLabel createStyledLabel(String text) 
+    {
+        JLabel label = new JLabel(text);
+
+        label.setFont(new Font("Arial", Font.BOLD, 14)); 
+        label.setForeground(Color.PINK);
+
+        return label;
     }
 }
