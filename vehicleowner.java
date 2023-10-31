4ffc0a9b52de014edadd1dca36c6a265cfb2145f
Steve Delgado <steve.delgado20@stjohns.edu>
2023-10-31 15:26:59 -0400

    Created a class for vehicleOwner

--- vehicleowner.java
+++ vehicleowner.java
@@ -1,3 +1,26 @@
 public class vehicleowner {
-    
+	private int ownerId;
+	private String name;
+
+	public vehicleowner(int ownerId, String name) {
+		this.ownerId = ownerId;
+		this.name = name;
+	}
+
+	public int getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
