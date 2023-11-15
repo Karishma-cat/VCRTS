b3268a2235fd93dbd877e60e5b53fd89746177b4
melgiler <146897278+melgiler@users.noreply.github.com>
2023-11-14 20:52:05 -0500

    up client.java

--- Client.java
+++ Client.java
@@ -26,44 +26,37 @@ class Client {
     
     public static void main(String[] args) {
 		
-		String dataIn = "";
-		String dataOut = "";
-		Scanner keyInput;
+		String messageIn = "";
+		String messageOut = "";
 		
 		try {
 
-			System.out.println("----------*** This is client side ***--------");
+			System.out.println("----*** This is client side ***----");
 			System.out.println("client started!");
+			
 			//connect the client socket to server
-			Socket socket = new Socket("localhost", 9806);
+			Socket socket = new Socket("localhost", 9808);
 			
 			
 			//client reads a message from Server
 			inputStream = new DataInputStream(socket.getInputStream());
 			outputStream = new DataOutputStream(socket.getOutputStream());
 			
-			
-			// client reads a message from keyboard
-			System.out.println("Enter the data you want to send to server side: ");
-			keyInput = new Scanner(System.in);
-			dataOut = keyInput.nextLine();
-			// server sends the message to client
-			outputStream.writeUTF(messageOut);
-			
 	
-			while(!dataIn.equals("exit")) {
-				dataIn = inputStream.readUTF();
+			while(!dataIn.equals("end")) {
+				
+			//	dataIn = inputStream.readUTF();
 				// client prints the message received from server to console
-				System.out.println("data received from server: " + "\"" + dataIn + "\"");
+			//	System.out.println("data received from server: " + "\"" + dataIn + "\"");
 				
 				
 				// ********************************************************
 				// client reads a message from keyboard
-				System.out.println("Enter a message you want to send to server side: ");
-				keyInput = new Scanner(System.in);
-				dataOut = keyInput.nextLine();
+			//	System.out.println("Enter a message you want to send to server side: ");
+			//	keyInput = new Scanner(System.in);
+			//	dataOut = keyInput.nextLine();
 				// server sends the message to client
-				outputStream.writeUTF(dataOut);
+			//	outputStream.writeUTF(dataOut);
 				
 			}
 						
