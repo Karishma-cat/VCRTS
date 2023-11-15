8be9d6f83281c1e2c4ecf88cafc4283a2d1ffa36
melgiler <146897278+melgiler@users.noreply.github.com>
2023-11-14 19:48:10 -0500

    Client.java to client-server

--- Client.java
+++ Client.java
@@ -1,4 +1,15 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.*;
+
 class Client {
+	static ServerSocket serverSocket;
+	static Socket socket;
+	static DataInputStream inputStream;
+	static DataOutputStream outputStream;
+
+	
     private String name;
 
     public Client(String name) {
@@ -12,5 +23,55 @@ class Client {
     public void setName(String name) {
         this.name = name;
     }
+    
+    public static void main(String[] args) {
+		
+		String dataIn = "";
+		String dataOut = "";
+		Scanner keyInput;
+		
+		try {
 
+			System.out.println("----------*** This is client side ***--------");
+			System.out.println("client started!");
+			//connect the client socket to server
+			Socket socket = new Socket("localhost", 9806);
+			
+			
+			//client reads a message from Server
+			inputStream = new DataInputStream(socket.getInputStream());
+			outputStream = new DataOutputStream(socket.getOutputStream());
+			
+			
+			// client reads a message from keyboard
+			System.out.println("Enter the data you want to send to server side: ");
+			keyInput = new Scanner(System.in);
+			dataOut = keyInput.nextLine();
+			// server sends the message to client
+			outputStream.writeUTF(messageOut);
+			
+	
+			while(!dataIn.equals("exit")) {
+				dataIn = inputStream.readUTF();
+				// client prints the message received from server to console
+				System.out.println("data received from server: " + "\"" + dataIn + "\"");
+				
+				
+				// ********************************************************
+				// client reads a message from keyboard
+				System.out.println("Enter a message you want to send to server side: ");
+				keyInput = new Scanner(System.in);
+				dataOut = keyInput.nextLine();
+				// server sends the message to client
+				outputStream.writeUTF(dataOut);
+				
+			}
+						
+
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+
+		}
+	}
 }
