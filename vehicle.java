c8f97f24f5929b5b0a32cc7a7ff5fe61cba4aed8
melgiler <146897278+melgiler@users.noreply.github.com>
2023-11-14 19:52:06 -0500

    Update vehicle.java

--- vehicle.java
+++ vehicle.java
@@ -1,8 +1,18 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.*;
+
 public class vehicle {
 // store the Vehicle ID
     private int vehicleID;
-// Store the owner of the vehocle
-private vehicleowner owner;
+// Store the owner of the vehicle
+    private vehicleowner owner;
+
+	static ServerSocket serverSocket;
+	static Socket socket;
+	static DataInputStream inputStream;
+	static DataOutputStream outputStream;
 
 // constructor to initialize the vehicle with a ID and owner
 public vehicle(int vehicleID, vehicleowner owner) {
@@ -32,5 +42,55 @@ public void arrive(){
 // method to show when vehicle departs
 public void depart(){
     System.out.println("Vehicle " + vehicleID + "has departed ");
+}
+public static void main(String[] args) {
+	
+	String messageIn = "";
+	String messageOut = "";
+	Scanner keyInput;
+	
+	try {
+
+		System.out.println("----------*** This is vehicle owner side ***--------");
+		System.out.println("vehicle owner started!");
+		//connect the client socket to server
+		Socket socket = new Socket("localhost", 9806);
+		
+		
+		//client reads a message from Server
+		inputStream = new DataInputStream(socket.getInputStream());
+		outputStream = new DataOutputStream(socket.getOutputStream());
+		
+		
+		// client reads a message from keyboard
+		System.out.println("Enter data you want to send to server side: ");
+		keyInput = new Scanner(System.in);
+		messageOut = keyInput.nextLine();
+		// server sends the message to client
+		outputStream.writeUTF(messageOut);
+		
+
+		while(!messageIn.equals("exit")) {
+			messageIn = inputStream.readUTF();
+			// client prints the message received from server to console
+			System.out.println("message received from server: " + "\"" + messageIn + "\"");
+			
+			
+			// ********************************************************
+			// client reads a message from keyboard
+			System.out.println("Enter a message you want to send to server side: ");
+			keyInput = new Scanner(System.in);
+			messageOut = keyInput.nextLine();
+			// server sends the message to client
+			outputStream.writeUTF(messageOut);
+			
+		}
+					
+
+	} catch (Exception e) {
+		
+		e.printStackTrace();
+
+	}
 }
 }
